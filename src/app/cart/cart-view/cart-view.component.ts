32d2ce26f3ec400ac9c332e6fdef3ac106e24003
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-08 00:10:13 +0100

    I used mat list for cart items

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from '../cart.service';
-import {Product} from 'src/app/models/product
+import {Product} from 'src/app/models/product'
 
 
 @Component({
