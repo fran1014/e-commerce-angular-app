02c0bae2c650403cea327087473954c23eccd26f
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-07 20:23:30 +0100

    import card module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { ProductModule } from './product/product.module';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button'
+import {MatButtonModule} from '@angular/material/button';
+import {CartModule} from './cart/cart.module'
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import {MatButtonModule} from '@angular/material/button'
     ProductModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    CartModule
     
   ],
   providers: [],
--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  private apiUrl = environment.apiUrl + "/cart";
 
   constructor() { }
 }
