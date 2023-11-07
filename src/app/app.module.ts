29ae136a04be538921a6dbb12585eaae81cb3126
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-07 19:50:38 +0100

    adding told nav menu in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     HttpClientModule,
     ProductModule,
     BrowserAnimationsModule,
+    MatToolbarModule
     
   ],
   providers: [],
