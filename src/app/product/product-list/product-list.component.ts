f976e627da373b571e83178fe5b2d3895cbfbbea
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-08 17:32:18 +0100

    added filtering functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = []
+  filteredProducts: Product [] = []
 
   constructor( private productService: ProductService, private cartService : CartService,
   private snackbar: MatSnackBar){}
@@ -33,6 +34,13 @@ export class ProductListComponent implements OnInit {
         })
       }
     });
+  }
+
+  applyFilter(event: Event): void{
+    let searchTerm = (event.target as HTMLInputElement).value;
+    searchTerm = searchTerm.toLowerCase ();
+
+    this.filteredProducts = this.products.filter(product => product.name.toLowerCase().includes(searchTerm))
   }
 
 }
